b460353a4c3d0e201141777ee58b0bd59e4f51c9
Roman Ohiienko <ohiienko.r@gmail.com>
2024-03-27 14:15:30 +0200

    feat: complete useRiddle custom hook

---
+++ src/Hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useRiddles } from "./useRiddles/index";
---
+++ src/Hooks/useRiddles/index.ts
@@ -0,0 +1,37 @@
+import { useTranslation } from "react-i18next";
+import { ROUTES_NAMES } from "@/Router/routes-names";
+
+const useRiddles = () => {
+  const { t } = useTranslation();
+
+  const riddles = {
+    first: {
+      id: 0,
+      image: "",
+      text: t("firstRiddleText"),
+      answerFormat: t("firstRiddleAnswerFormat"),
+      hints: ["", "", ""],
+      nextStep: ROUTES_NAMES.SECOND_RIDDLE,
+    },
+    second: {
+      id: 1,
+      image: "",
+      text: t("secondRiddleText"),
+      answerFormat: t("secondRiddleAsnwerFormat"),
+      hints: ["", "", ""],
+      nextStep: ROUTES_NAMES.THIRD_RIDDLE,
+    },
+    third: {
+      id: 2,
+      image: "",
+      text: t("thirdRiddleText"),
+      answerFormat: t("thirdRiddleAnswerformat"),
+      hints: ["", "", ""],
+      nextStep: ROUTES_NAMES.END,
+    },
+  };
+
+  return { riddles };
+};
+
+export default useRiddles;
