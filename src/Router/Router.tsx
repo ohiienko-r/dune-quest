062b265176ca3c5030082a957f8465f885f5a88d
Roman Ohiienko <ohiienko.r@gmail.com>
2024-03-30 00:23:41 +0200

    fix: add basename to browser router for deploy

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -8,66 +8,69 @@ import { Hero, Introduction, Riddle, DemoEnd } from "@/Pages";
 const Router = () => {
   const { riddles } = useRiddles();
 
-  const router = createBrowserRouter([
-    {
-      path: ROUTES_NAMES.HOME,
-      element: <Root />,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <Hero />,
-        },
-        {
-          path: ROUTES_NAMES.INTRODUCTION,
-          element: <Introduction />,
-        },
-        {
-          path: ROUTES_NAMES.FIRST_RIDDLE,
-          element: (
-            <Riddle
-              id={riddles.first.id}
-              image={riddles.first.image}
-              text={riddles.first.text}
-              answerFormat={riddles.first.answerFormat}
-              hints={riddles.first.hints}
-              nextRoute={riddles.first.nextStep}
-            />
-          ),
-        },
-        {
-          path: ROUTES_NAMES.SECOND_RIDDLE,
-          element: (
-            <Riddle
-              id={riddles.second.id}
-              image={riddles.second.image}
-              text={riddles.second.text}
-              answerFormat={riddles.second.answerFormat}
-              hints={riddles.second.hints}
-              nextRoute={riddles.second.nextStep}
-            />
-          ),
-        },
-        {
-          path: ROUTES_NAMES.THIRD_RIDDLE,
-          element: (
-            <Riddle
-              id={riddles.third.id}
-              image={riddles.third.image}
-              text={riddles.third.text}
-              answerFormat={riddles.third.answerFormat}
-              hints={riddles.third.hints}
-              nextRoute={riddles.third.nextStep}
-            />
-          ),
-        },
-        {
-          path: ROUTES_NAMES.END,
-          element: <DemoEnd />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: ROUTES_NAMES.HOME,
+        element: <Root />,
+        errorElement: <Error />,
+        children: [
+          {
+            index: true,
+            element: <Hero />,
+          },
+          {
+            path: ROUTES_NAMES.INTRODUCTION,
+            element: <Introduction />,
+          },
+          {
+            path: ROUTES_NAMES.FIRST_RIDDLE,
+            element: (
+              <Riddle
+                id={riddles.first.id}
+                image={riddles.first.image}
+                text={riddles.first.text}
+                answerFormat={riddles.first.answerFormat}
+                hints={riddles.first.hints}
+                nextRoute={riddles.first.nextStep}
+              />
+            ),
+          },
+          {
+            path: ROUTES_NAMES.SECOND_RIDDLE,
+            element: (
+              <Riddle
+                id={riddles.second.id}
+                image={riddles.second.image}
+                text={riddles.second.text}
+                answerFormat={riddles.second.answerFormat}
+                hints={riddles.second.hints}
+                nextRoute={riddles.second.nextStep}
+              />
+            ),
+          },
+          {
+            path: ROUTES_NAMES.THIRD_RIDDLE,
+            element: (
+              <Riddle
+                id={riddles.third.id}
+                image={riddles.third.image}
+                text={riddles.third.text}
+                answerFormat={riddles.third.answerFormat}
+                hints={riddles.third.hints}
+                nextRoute={riddles.third.nextStep}
+              />
+            ),
+          },
+          {
+            path: ROUTES_NAMES.END,
+            element: <DemoEnd />,
+          },
+        ],
+      },
+    ],
+    { basename: ROUTES_NAMES.HOME }
+  );
 
   return <RouterProvider router={router} />;
 };
